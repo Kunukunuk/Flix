048534902d20a1845a50749e1c9d58fdba612ae4
Kun Huang <kkun.huang@gmail.com>
2018-09-11 22:28:10 -0400

    collection view setup

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Kun Huang on 9/11/18.
+//  Copyright © 2018 Kun Huang. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
