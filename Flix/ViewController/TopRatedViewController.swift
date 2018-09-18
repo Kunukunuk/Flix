88a348ef51c635cde7d2a966dc1aba38bfd06db9
Kun Huang <kkun.huang@gmail.com>
2018-09-17 20:27:44 -0400

    top rated movies

--- Flix/ViewController/TopRatedViewController.swift
+++ Flix/ViewController/TopRatedViewController.swift
@@ -1,25 +1,23 @@
 //
-//  SuperheroViewController.swift
+//  TopRatedViewController.swift
 //  Flix
 //
-//  Created by Kun Huang on 9/11/18.
+//  Created by Kun Huang on 9/17/18.
 //  Copyright © 2018 Kun Huang. All rights reserved.
 //
 
 import UIKit
 
-class SuperheroViewController: UIViewController, UICollectionViewDataSource{
-    
+class TopRatedViewController: UIViewController, UICollectionViewDataSource {
+
     @IBOutlet weak var collectionView: UICollectionView!
     var movies: [[String: Any]] = []
-    var genreID = 28
-    var genreName = "Superhero"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        //NotificationCenter.default.addObserver(self, selector: #selector(loadList), name: NSNotification.Name(rawValue: "load"), object: nil)
         movies = []
-        self.navigationItem.title = "\(genreName) Movies"
+        //self.navigationItem.title = "\(genreName) Movies"
         
         collectionView.dataSource = self
         
@@ -34,7 +32,7 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource{
         fetchMovies()
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -43,21 +41,21 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return movies.count
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath) as! PosterCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TopRated", for: indexPath) as! TopRatedCell
         let movie = movies[indexPath.row]
         if let posterPath = movie["poster_path"] as? String {
             let baseURL = "https://image.tmdb.org/t/p/w500"
             let posterURL = URL(string: baseURL + posterPath)!
-            cell.posterImageView.af_setImage(withURL: posterURL)
+            cell.topRatedImage.af_setImage(withURL: posterURL)
         }
         return cell
     }
     
     func fetchMovies() {
         
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/top_rated?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
@@ -68,18 +66,9 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource{
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 let movies = dataDictionary["results"] as! [[String: Any]]
-                for movie in movies {
-                    let genre = movie["genre_ids"]! as! NSArray
-                    if genre.contains(self.genreID) {
-                        self.movies.append(movie)
-                    }
-                
-                }
+                self.movies = movies
                 //self.movies = movies
                 self.collectionView.reloadData()
-                //self.refreshControl.endRefreshing()
-               // self.activity.stopAnimating()
-                //self.filteredData = self.movies
                 
             }
         }
@@ -89,17 +78,14 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource{
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        if (segue.identifier == "GoToDetails") {
+        if (segue.identifier == "GoToMovie") {
             let cell = sender as! UICollectionViewCell
             if let indexPath = collectionView.indexPath(for: cell) {
                 let movie = movies[indexPath.row]
                 let detailViewController = segue.destination as! DetailsViewController
                 detailViewController.movie = movie
             }
-        }// else if (segue.identifier == "PopUpModal") {
-        //}
-        
+        }
     }
-    
-}
 
+}
---
+++ Flix/Views/TopRatedCell.swift
@@ -0,0 +1,15 @@
+//
+//  TopRatedCell.swift
+//  Flix
+//
+//  Created by Kun Huang on 9/17/18.
+//  Copyright © 2018 Kun Huang. All rights reserved.
+//
+
+import UIKit
+
+class TopRatedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var topRatedImage: UIImageView!
+    
+}
